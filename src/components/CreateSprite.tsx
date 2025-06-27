d8443f03cc492446595d093bc819d6c614a35d54
Danny Barrett <dannybarrett1992@outlook.com>
2025-06-27 15:07:12 -0700

    Fixed default colors being in the wrong order.

--- src/components/CreateSprite.tsx
+++ src/components/CreateSprite.tsx
@@ -29,8 +29,8 @@ export default function CreateSprite() {
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     const defaultColors = [
-      { r: 255, g: 255, b: 255, a: 255 },
       { r: 0, g: 0, b: 0, a: 255 },
+      { r: 255, g: 255, b: 255, a: 255 },
     ];
 
     const newSprite: Sprite = {
