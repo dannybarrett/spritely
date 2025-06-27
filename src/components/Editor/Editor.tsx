d3307d89f95b2f27f85c7c551388eebf6e5b8fe8
Danny Barrett <dannybarrett1992@outlook.com>
2025-06-27 15:10:56 -0700

    Added canvas to sprite editor.

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,7 +1,15 @@
+import { useRef } from "react";
+
 export default function Editor() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   return (
     <div className="h-full w-full grid place-items-center">
-      <p>editor</p>
+      <canvas
+        ref={canvasRef}
+        width="196"
+        height="196"
+        className="border"
+      ></canvas>
     </div>
   );
 }
