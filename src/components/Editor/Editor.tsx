08b8553085a9b4beffcc579d86da510d71bef131
Danny Barrett <dannybarrett1992@outlook.com>
2025-06-27 15:36:40 -0700

    Fixed incorrect parent element for canvas. Updated canvas to resize on window resize.

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -5,17 +5,15 @@ export default function Editor() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const sprite = useSpriteStore((state: SpriteState) => state.sprite);
 
-  useEffect(() => {
-    console.log("sprite", sprite);
+  function scaleCanvas() {
     const canvas = canvasRef.current;
 
     if (!canvas || !sprite) return;
     const parentSize = canvas.parentElement?.getBoundingClientRect();
 
-    console.log("parent", parentSize);
-    // parent width = 1024, h = 650;
-    // sprite size = 24x16
-    const smallestParentSide = Math.min(parent.innerWidth, parent.innerHeight); // biggest side we can have for the finished canvas
+    if (!parentSize) return;
+
+    const smallestParentSide = Math.min(parentSize.width, parentSize.height); // biggest side we can have for the finished canvas
     const smallestSpriteSide = Math.min(
       sprite.width || 16,
       sprite.height || 16
@@ -25,6 +23,18 @@ export default function Editor() {
 
     canvas.width = Math.floor(sprite.width * pixelSize * 0.75);
     canvas.height = Math.floor(sprite.height * pixelSize * 0.75);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", scaleCanvas);
+
+    return () => {
+      window.removeEventListener("resize", scaleCanvas);
+    };
+  }, []);
+
+  useEffect(() => {
+    scaleCanvas();
   }, [canvasRef, sprite]);
 
   return (
