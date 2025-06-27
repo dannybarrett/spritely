3a1d26c0fd137a9de5c3fde5043d77d84fb862f2
Danny Barrett <dannybarrett1992@outlook.com>
2025-06-27 13:30:58 -0700

    Added color selector and util function to convert hex to rgba

--- src/components/Editor/Colors.tsx
+++ src/components/Editor/Colors.tsx
@@ -1,42 +1,89 @@
 import { SpriteState, useSpriteStore } from "@/stores/spriteStore";
 import { Button } from "../ui/button";
+import { Plus } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import ColorPicker from "@rc-component/color-picker";
+import "@rc-component/color-picker/assets/index.css";
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Sprite } from "@/types/sprite";
+import { hexToRgba } from "@/lib/utils";
+
+function ColorSelector({ onAdd }: { onAdd: (c: string) => void }) {
+  const [color, setColor] = useState<string>("#ffffff");
+  return (
+    <div>
+      <ColorPicker
+        value={color}
+        onChange={value => setColor(value.toHexString())}
+      />
+      <Input value={color} onChange={event => setColor(event.target.value)} />
+      <Button onClick={() => onAdd(color)}>Add color</Button>
+    </div>
+  );
+}
 
 export default function Colors() {
-  const colors = useSpriteStore((state: SpriteState) => state.sprite?.colors);
+  const sprite = useSpriteStore((state: SpriteState) => state.sprite);
+  const setSprite = useSpriteStore((state: SpriteState) => state.setSprite);
   const color = useSpriteStore((state: SpriteState) => state.color);
   const setColor = useSpriteStore((state: SpriteState) => state.setColor);
   const altColor = useSpriteStore((state: SpriteState) => state.altColor);
   const setAltColor = useSpriteStore((state: SpriteState) => state.setAltColor);
 
+  const [colorSelectorOpen, setColorSelectorOpen] = useState(false);
+
+  if (!sprite) return <>error: no sprite</>;
+
   return (
     <section className="border-l p-2">
       <div className="grid grid-cols-2 gap-2">
-        {colors &&
-          colors.map(c => (
-            <Button
-              variant="outline"
-              size="icon"
-              style={{
-                background: `rgb(${c.r} ${c.g} ${c.b} / ${c.a})`,
-              }}
-              onMouseDown={event => {
-                if (event.buttons === 1) {
-                  setColor(c);
-                }
-                if (event.buttons === 2) {
-                  setAltColor(c);
-                }
-              }}
-              className="relative z-10 border-none"
-            >
-              {c === color && (
-                <div className="border border-neutral-600 border-t-0 bg-neutral-800 w-2/5 h-2/5 absolute top-0 left-0 rounded-tl-sm border-l-0" />
-              )}
-              {c === altColor && (
-                <div className="border border-neutral-600 border-t-0 bg-neutral-800 w-2/5 h-2/5 absolute top-0 right-0 rounded-tr-sm border-r-0" />
-              )}
+        <Popover open={colorSelectorOpen} onOpenChange={setColorSelectorOpen}>
+          <PopoverTrigger>
+            <Button variant="outline" size="icon">
+              <Plus />
             </Button>
-          ))}
+          </PopoverTrigger>
+          <PopoverContent>
+            <ColorSelector
+              onAdd={(c: string) => {
+                setColorSelectorOpen(false);
+                const newColor = hexToRgba(c);
+                const newSprite: Sprite = {
+                  ...sprite,
+                  colors: [...sprite.colors, newColor],
+                };
+                setSprite(newSprite);
+              }}
+            />
+          </PopoverContent>
+        </Popover>
+        {sprite.colors.map((c, i) => (
+          <Button
+            key={i}
+            variant="outline"
+            size="icon"
+            style={{
+              background: `rgb(${c.r} ${c.g} ${c.b} / ${c.a})`,
+            }}
+            onMouseDown={event => {
+              if (event.buttons === 1) {
+                setColor(c);
+              }
+              if (event.buttons === 2) {
+                setAltColor(c);
+              }
+            }}
+            className="relative z-10 border-none"
+          >
+            {c === color && (
+              <div className="border border-neutral-600 border-t-0 bg-neutral-800 w-2/5 h-2/5 absolute top-0 left-0 rounded-tl-sm border-l-0" />
+            )}
+            {c === altColor && (
+              <div className="border border-neutral-600 border-t-0 bg-neutral-800 w-2/5 h-2/5 absolute top-0 right-0 rounded-tr-sm border-r-0" />
+            )}
+          </Button>
+        ))}
       </div>
     </section>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Pixel } from "@/types/sprite";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function hexToRgba(hex: string): Pixel {
+  const arr = hex.split("");
+  arr.splice(0, 1); // remove '#' from beginning of string
+
+  const r = parseInt(arr.slice(0, 2).join(""), 16);
+  const g = parseInt(arr.slice(2, 4).join(""), 16);
+  const b = parseInt(arr.slice(4, 6).join(""), 16);
+  const a = arr.length === 8 ? parseInt(arr.slice(6, 8).join(""), 16) : 255;
+
+  return { r, g, b, a };
 }
