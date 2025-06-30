e16be64dcf01182b4d0aa8cbe0721c7e2677e6ab
Danny Barrett <dannybarrett1992@outlook.com>
2025-06-29 18:57:24 -0700

    Updated window title to change based on sprite state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 import "./App.css";
 import CreateSprite from "./components/CreateSprite";
 import Editor from "./components/Editor/Editor";
 import EditorLayout from "./components/Editor/EditorLayout";
 import { SpriteState, useSpriteStore } from "./stores/spriteStore";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
 function App() {
   const sprite = useSpriteStore((state: SpriteState) => state.sprite);
+  const history = useSpriteStore((state: SpriteState) => state.history);
+  const appWindow = getCurrentWindow();
+
+  useEffect(() => {
+    async function updateTitle() {
+      if (sprite) {
+        await appWindow.setTitle(
+          `Spritely - ${sprite.name}${
+            history.prev.length > 0 || history.next.length > 0 ? "*" : ""
+          }`
+        );
+      }
+    }
+
+    updateTitle();
+  }, [sprite, appWindow]);
 
   if (!sprite) {
     return (
